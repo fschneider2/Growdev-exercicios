e7ac8fb4c3b0f3fc4147e59a5f0683cdc6af2b8c
Dev <dev@pop-os.localdomain>
2022-05-24 20:13:46 -0300

    adicionado exercicio 29

--- exercicio_29.py
+++ exercicio_29.py
@@ -3,28 +3,28 @@
 # resulta em três moedas de um real, uma de cinquenta centavos, duas de dez centavos, uma de 
 # 5 centavos e três de um centavo.
 valor = float(input('Informe o valor: R$'))
-moeda_1 = valor / 100
-resto_1 = valor % 100
-moeda_050 = resto_1 / 50
-resto_050 = resto_1 % 50
-moeda_025 = resto_050 / 25
-resto_025 = resto_050 % 25
-moeda_010 = resto_025 / 10
-resto_010 = resto_025 % 10
-moeda_005 = resto_010 / 5
-resto_005 = resto_010 % 5
-moeda_001 = resto_005 / 1
-resto_001 = resto_005 % 1
+moeda_1 = valor / 1
+resto_1 = valor % 1
+moeda_050 = resto_1 / 0.50
+resto_050 = resto_1 % 0.50
+moeda_025 = resto_050 / 0.25
+resto_025 = resto_050 % 0.25
+moeda_010 = resto_025 / 0.10
+resto_010 = resto_025 % 0.10
+moeda_005 = resto_010 / 0.05
+resto_005 = resto_010 % 0.05
+moeda_001 = resto_005 / 0.01
+resto_001 = resto_005 % 0.01
 if(moeda_1 > 0):
-    print('Moedas de R$ 1,00 = ',moeda_1)
+    print('Moedas de R$ 1,00 = ',int(moeda_1))
 if(moeda_050 > 0):
-    print('Moedas de R$ 0,50 = ',moeda_050)
+    print('Moedas de R$ 0,50 = ',int(moeda_050))
 if(moeda_025 > 0):
-    print('Moedas de R$ 0,25 = ',moeda_025)
+    print('Moedas de R$ 0,25 = ',int(moeda_025))
 if(moeda_010 > 0):
-    print('Moedas de R$ 0,10 = ',moeda_010)
+    print('Moedas de R$ 0,10 = ',int(moeda_010))
 if(moeda_005 > 0):
-    print('Moedas de R$ 0,05 = ',moeda_005)
+    print('Moedas de R$ 0,05 = ',int(moeda_005))
 if(moeda_001 > 0):
-    print('Moedas de R$ 0,01 = ',moeda_001)
+    print('Moedas de R$ 0,01 = ',int(moeda_001))
 
