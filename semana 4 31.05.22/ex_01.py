a82958ac6daa2702dfcd4eceae049f33da547997
Dev <dev@pop-os.localdomain>
2022-06-02 22:17:44 -0300

    Correção exercicio 3, semana 4

--- semana 4 31.05.22/ex_01.py
+++ semana 4 31.05.22/ex_01.py
@@ -3,9 +3,12 @@
 
 # sempre usando o contador, para finalizar o laço de repetição e em alguns 
 # casos para armazer a saida que queremos, nesse caso ele informa quantos dos numeros são negativos
+
 contador = 0
+
 for a in range(5):
     a = int(input('Digite um numero: '))
     if a < 0:
         contador += 1
+
 print(f'{contador} dos numeros digitados são negativos')
--- semana 4 31.05.22/ex_03.py
+++ semana 4 31.05.22/ex_03.py
@@ -5,13 +5,15 @@
 # entrada dados
 n = int(input('Informe um numero maior que 0: '))
 
-# laço de repetição para evitar que usuario insira numero negativo, se for inserido um float, codigo quebra.
-while not (n > 0):
-    n = int(input('Informe um numero maior que 0: '))
+somatorio = 1
 
-# usei o for para criar um laço de repetição, que irá de 1 até o n° anterior a n e armazei isso em x
-# dessa forma, ao criar uma variavel e e adicionar a ela 1, teremos a resposta do exercicio. Se n == 3 :
-# E = (0 + 1 / 1!) (1! + 1 / 2! (1! é a resposta de 1 + x)) (2! + 1 / 3!) 
-for x in range(0, n):
-    e = 1 + x
-    print(f'E = {x} + 1 / {e}!')
+
+
+
+for x in range(1, n + 1):
+    fatorial = 1
+    for valor in range(x, 1, -1):
+        fatorial = fatorial * valor
+    somatorio = somatorio +1/ fatorial
+
+print(f'E = {somatorio}')
